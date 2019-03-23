b091cd48d189b55e1bf5498b88a78071f3db28e9
Joel Chen <xchen@walmartlabs.com>
2019-03-23 15:21:31 -0700

    test: onError for proxy ECONNREFUSED

--- test/test_hooks.js
+++ test/test_hooks.js
@@ -6,7 +6,7 @@ const electrodeServer = require('electrode-server');
 const needle = require('needle');
 const { expect } = require('chai');
 
-describe('onRequest/onResponse hooks', function() {
+describe('hooks', function() {
   function setupTestRoute(handler) {
     return electrodeServer().then(server => {
       server.route({
@@ -18,7 +18,7 @@ describe('onRequest/onResponse hooks', function() {
     });
   }
 
-  it('should be able to modify headers for a route', () => {
+  it('onRequest should be able to modify headers for a route', () => {
     let server;
     let proxy;
     let serverReq;
@@ -68,7 +68,7 @@ describe('onRequest/onResponse hooks', function() {
     );
   });
 
-  it('should be able skip forwarding by returning false target', () => {
+  it('onRequest should be able skip forwarding by returning false target', () => {
     let server;
     let proxy;
     let serverReq;
@@ -119,6 +119,50 @@ describe('onRequest/onResponse hooks', function() {
       },
       runFinally(() => proxy && proxy.close()),
       runFinally(() => server && server.stop())
+    );
+  });
+
+  it('onError should be invoked for proxy errors', () => {
+    let proxy;
+    let proxyReq;
+    let proxyError;
+    let saveProxyHeaders;
+    let target;
+
+    return asyncVerify(
+      () => {
+        proxy = redbird({ bunyan: false, port: 18999 });
+        proxy.register({
+          src: 'localhost/x',
+          target: 'http://localhost:3000/test',
+          onRequest: (req, res, tgt) => {
+            proxyReq = req;
+            saveProxyHeaders = req.headers;
+            target = tgt;
+          },
+          onResponse: (req, res, tgt) => {
+            responseTarget = tgt;
+          },
+          onError: (err, req, res) => {
+            proxyError = err;
+            proxy.handleProxyError(err, req, res);
+          }
+        });
+        return needle('get', 'http://localhost:18999/x', {
+          headers: {
+            blah: 'xyz'
+          }
+        });
+      },
+      res => {
+        expect(res.statusCode).to.equal(502);
+        expect(target).to.exist;
+        expect(saveProxyHeaders).to.exist;
+        expect(saveProxyHeaders.blah).to.equal('xyz');
+        expect(proxyError).to.exist;
+        expect(proxyError.code).to.equal('ECONNREFUSED');
+      },
+      runFinally(() => proxy && proxy.close())
     );
   });
 });
